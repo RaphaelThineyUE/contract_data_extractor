aafbfe3d69093fc49c015e7ec0a55c85aac7fefa
Raphael Thiney <raphael.thiney@unisonenergy.com>
2024-09-06 19:11:51 -0400

    before nx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contract_data_extractor';
+  title = 'Contract Data Extractor';
 }
